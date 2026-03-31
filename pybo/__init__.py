53fc4c0ad4a9c28f4250d1e9f421b2ee3d3c1f0c
lwh-a <nananatehan1133@naver.com>
2026-03-31 13:12:56 +0900

    20260331

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
     # 블루포인트 등록
     from .views import main_views
